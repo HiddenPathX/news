eef0ff4f3f95f50d956b2a982bc06f129d704a9e
HiddenPathX <wisdomxxxxxx@gmail.com>
2025-01-06 02:50:13 +0800

    Add files via upload

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,13 +1,5 @@
 export function Footer() {
   return (
-    <>
-      <a href={`${Homepage}/LICENCE`} target="_blank">MIT LICENCE</a>
-      <span>
-        <span>NewsNow © 2024 By </span>
-        <a href={Author.url} target="_blank">
-          {Author.name}
-        </a>
-      </span>
-    </>
+    <span>© 2025 番茄工作法 | 专注高效，掌控时间</span>
   )
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -6,17 +6,11 @@ export function NavBar() {
   const currentId = useAtomValue(currentColumnIDAtom)
   const { toggle } = useSearchBar()
   return (
-    <span className={$([
-      "flex p-3 rounded-2xl bg-primary/1 text-sm",
-      "shadow shadow-primary/20 hover:shadow-primary/50 transition-shadow-500",
-    ])}
-    >
+    <div className="navigation">
       <button
         type="button"
         onClick={() => toggle(true)}
-        className={$(
-          "px-2 hover:(bg-primary/10 rounded-md) op-70 dark:op-90",
-        )}
+        className="nav-button"
       >
         更多
       </button>
@@ -25,14 +19,11 @@ export function NavBar() {
           key={columnId}
           to="/c/$column"
           params={{ column: columnId }}
-          className={$(
-            "px-2 hover:(bg-primary/10 rounded-md)",
-            currentId === columnId ? "color-primary font-bold" : "op-70 dark:op-90",
-          )}
+          className={currentId === columnId ? "active" : ""}
         >
           {metadata[columnId].name}
         </Link>
       ))}
-    </span>
+    </div>
   )
 }
